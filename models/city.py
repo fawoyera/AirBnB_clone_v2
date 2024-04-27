402c808225f35908657f7b7cf9c4f8a2d4f69441
fawoyera <femiawoyera@gmail.com>
2024-04-27 11:38:51 -0700

    update class Place with table attributes

--- models/city.py
+++ models/city.py
@@ -3,6 +3,7 @@
 from models.base_model import BaseModel, Base
 from models.state import State
 from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
@@ -10,3 +11,6 @@ class City(BaseModel, Base):
     __tablename__ = "cities"
     state_id = Column(String(60), ForeignKey(State.id), nullable=False)
     name = Column(String(128), nullable=False)
+
+    places = relationship('Place', cascade="all, delete, delete-orphan",
+                          backref="cities")
