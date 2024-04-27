694613bf8921556d6b02d5b4c1f3b0bccd9d91a8
fawoyera <femiawoyera@gmail.com>
2024-04-27 09:07:28 -0700

    implement DBStorage for States and Cities

---
+++ models/engine/db_storage.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python3
+"""DBStorage Module that defines a class to manage database storage for hbnb"""
+
+
+class DBStorage:
+    """Class to define the new DB storage"""
+    __engine = None
+    __session = None
+
+    def __init__(self):
+        """Initialization method for DBStorage instance"""
+        from models.base_model import BaseModel, Base
+        from models.city import City
+        from models.state import State
+        import os
+        from sqlalchemy import create_engine
+
+        HBNB_MYSQL_USER = os.environ.get('HBNB_MYSQL_USER')
+        HBNB_MYSQL_PWD = os.environ.get('HBNB_MYSQL_PWD')
+        HBNB_MYSQL_HOST = os.environ.get('HBNB_MYSQL_HOST')
+        HBNB_MYSQL_DB = os.environ.get('HBNB_MYSQL_DB')
+        HBNB_ENV = os.environ.get('HBNB_ENV')
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
+                                      .format(HBNB_MYSQL_USER, HBNB_MYSQL_PWD,
+                                              HBNB_MYSQL_HOST, HBNB_MYSQL_DB),
+                                      pool_pre_ping=True)
+
+        if HBNB_ENV == "test":
+            Base.metadata.drop_all(self.__engine)
+
+    def all(self, cls=None):
+        """Query on the current database session for all objects in a class"""
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.place import Place
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.review import Review
+
+        classes = {
+                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
+                    'State': State, 'City': City, 'Amenity': Amenity,
+                    'Review': Review
+                  }
+
+        if cls is None:
+            query_results = self.__session.query(User, State, City, Place,
+                                                 Amenity, Review).all()
+        else:
+            if cls.__name__ in classes:
+                query_results = self.__session.query(cls.__name__).all()
+
+        return {obj.__class__.__name__ + '.' + obj.id: obj for
+                obj in query_results}
+
+    def new(self, obj):
+        """Addd object to the current database session"""
+        self.__session.add(obj)
+
+    def save(self):
+        """Commit all changes of the current database session"""
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        """Delete object from the current database session if not None"""
+        if obj is not None:
+            self.__session.delete(obj)
+
+    def reload(self):
+        """Create all tables in the database"""
+        from models.base_model import BaseModel, Base
+        from models.user import User
+        from models.state import State
+        from models.city import City
+        from models.place import Place
+        from models.amenity import Amenity
+        from models.review import Review
+        import os
+        from sqlalchemy import create_engine
+        from sqlalchemy.orm import sessionmaker, scoped_session
+        from sqlalchemy.ext.declarative import declarative_base
+
+        Base.metadata.create_all(self.__engine)
+        session_factory = sessionmaker(bind=self.__engine,
+                                       expire_on_commit=False)
+        Session = scoped_session(session_factory)
+        self.__session = Session()
--- models/state.py
+++ models/state.py
@@ -1,8 +1,20 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    from models.engine.file_storage import FileStorage
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+
+    cities = relationship("City", cascade="all, delete, delete-orphan",
+                          backref="state")
+
+    @property
+    def cities(self):
+        return [city for city in FileStorage.__objects
+                if (city.split(".")[0] == "City" and city.state_id == self.id)]
