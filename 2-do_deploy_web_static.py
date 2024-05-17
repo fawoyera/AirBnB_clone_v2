bdc8ce785c9b10a9ee516852f0275741567be9ca
fawoyera <femiawoyera@gmail.com>
2024-05-17 01:14:28 +0000

    add fabric script to distribute archive to the web servers

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -16,21 +16,14 @@ def do_deploy(archive_path):
         archive_file_nopath = os.path.basename(archive_path)
         archive_path_noext = os.path.splitext(archive_file_nopath)[0]
         r1 = put('{}'.format(archive_path), '/tmp')
-        r2 = run('mkdir -p /data/web_static/\
-                  releases/{}/'.format(archive_path_noext))
-        r3 = run('tar -xzf /tmp/{}.tgz -C /data/web_static/\
-                  releases/{}/'.format(archive_path_noext, archive_path_noext))
+        r2 = run('mkdir -p /data/web_static/releases/{}/'.format(archive_path_noext))
+        r3 = run('tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/'.format(archive_path_noext, archive_path_noext))
         r4 = run('rm /tmp/{}.tgz'.format(archive_path_noext))
-        r5 = run('mv /data/web_static/releases/{}/web_static/* \
-                  /data/web_static/releases/{}/'.format(archive_path_noext,
-                                                        archive_path_noext))
-        r6 = run('rm -rf /data/web_static/releases/{}/\
-                  web_static'.format(archive_path_noext))
+        r5 = run('mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/'.format(archive_path_noext, archive_path_noext))
+        r6 = run('rm -rf /data/web_static/releases/{}/web_static'.format(archive_path_noext))
         r7 = run('rm -rf /data/web_static/current')
-        r8 = run('ln -s /data/web_static/releases/{}/ \
-                  /data/web_static/current'.format(archive_path_noext))
-        if r1.succeeded and r2.succeeded and r3.succeeded and r4.succeeded and\
-           r5.succeeded and r6.succeeded and r7.succeeded and r8.succeeded:
+        r8 = run('ln -s /data/web_static/releases/{}/ /data/web_static/current'.format(archive_path_noext))
+        if r1.succeeded and r2.succeeded and r3.succeeded and r4.succeeded and r5.succeeded and r6.succeeded and r7.succeeded and r8.succeeded:
             return True
         else:
             return False
