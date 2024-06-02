92d09b005fe83867e6a8386d0b3f5cc13032a671
fawoyera <femiawoyera@gmail.com>
2024-06-02 13:37:51 -0700

    add route for variable text

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+    Module script to start a Flask web application
+"""
+from flask import Flask
+from markupsafe import escape
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """default route"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """hbnb route"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """c route"""
+    modified_text = ""
+    for char in escape(text):
+        if char == "_":
+            modified_text += " "
+        else:
+            modified_text += char
+    return "C " + modified_text
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
